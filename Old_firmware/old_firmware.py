480640f247a3dc89c6b145da2a34ef5a3fdcbbbf
Saujeet Sarkar <teamsoftware93@gmail.com>
2024-03-27 19:00:46 +0530

    updated

--- Old_firmware/old_firmware.py
+++ Old_firmware/old_firmware.py
@@ -80,19 +80,19 @@ def job():
     print(f"Automation scheduling started at: {ist_now} IST time")
 
     # Schedule task to turn on AC daily at 8 AM and 9:30 PM UTC+05:30
-    schedule.every().day.at("02:30").do(read_and_send_messages, 'E:/Saujeet/Diebold/Automation_For_AC/Old_firmware/ALL_SITES(8_TO_10).csv', False, client)
-    schedule.every().day.at("16:30").do(read_and_send_messages, 'E:/Saujeet/Diebold/Automation_For_AC/Old_firmware/ALL_SITES(8_TO_10).csv', True, client)
+    schedule.every().day.at("02:30").do(read_and_send_messages, 'ALL_SITES(8_TO_10).csv', False, client)
+    schedule.every().day.at("16:30").do(read_and_send_messages, 'ALL_SITES(8_TO_10).csv', True, client)
 
     # Schedule hourly execution to turn on AC after 08:00 am till 10:00 pm UTC+05:30 for ALL_SITES
     for hour in range(3, 16):
-        schedule.every().day.at(f"{hour:02}:30").do(read_and_send_messages, 'E:/Saujeet/Diebold/Automation_For_AC/Old_firmware/ALL_SITES(8_TO_10).csv', False, client)
+        schedule.every().day.at(f"{hour:02}:30").do(read_and_send_messages, 'ALL_SITES(8_TO_10).csv', False, client)
 
     # Schedule hourly execution to turn off AC after 20:00 for ALL_SITES
     for hour in range(17, 24):
-        schedule.every().day.at(f"{hour:02}:30").do(read_and_send_messages, 'E:/Saujeet/Diebold/Automation_For_AC/Old_firmware/ALL_SITES(8_TO_10).csv', True, client)
+        schedule.every().day.at(f"{hour:02}:30").do(read_and_send_messages, 'ALL_SITES(8_TO_10).csv', True, client)
     for hour in range(0, 2):
-        schedule.every().day.at(f"{hour:02}:30").do(read_and_send_messages, 'E:/Saujeet/Diebold/Automation_For_AC/Old_firmware/ALL_SITES(8_TO_10).csv', True, client)
-
+        schedule.every().day.at(f"{hour:02}:30").do(read_and_send_messages, 'ALL_SITES(8_TO_10).csv', True, client)
+    
     while True:
         schedule.run_pending()
         time.sleep(5)  # Check every 5 seconds
